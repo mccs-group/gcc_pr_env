68f5d2b23b8369326d7723e88a3bb094bc524170
Zakhar Akimov <azd.inform@gmail.com>
2023-07-24 14:56:44 +0300

    added true benching

--- datasets/cbench.py
+++ datasets/cbench.py
@@ -4,14 +4,14 @@ from typing import Iterable
 class CBenchDataset(Dataset):
     def __init__(self, *args, **kwargs):
         super().__init__(
-            name="benchmark://gcc_pr-cbench",
+            name="benchmark://cbench",
             license="N/A",
             description="CBench 1.1 dataset",
             validatable="Yes",
         )
 
     def benchmark_uris(self) -> Iterable[str]:
-        return ["Whatever you want"]
+        return ["Hippety Hopper"]
 
     def benchmark_from_parsed_uri(self, uri: BenchmarkUri) -> Benchmark:
         return Benchmark.from_file_contents(uri, None)
--- service/gcc_pr_service.py
+++ service/gcc_pr_service.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/python3
 import logging
 from pathlib import Path
 from typing import Optional, Tuple
@@ -11,9 +11,12 @@ from compiler_gym.service.proto import (
         ObservationSpace,
         DoubleRange,
         Int64Range,
-        Benchmark,
         )
 from compiler_gym.service.runtime import create_and_run_compiler_gym_service
+from shutil import copytree
+from compiler_gym.datasets import (BenchmarkUri, Benchmark)
+from subprocess import *
+from time import *
 
 class GccPRCompilationSession(CompilationSession):
 
@@ -59,10 +62,14 @@ class GccPRCompilationSession(CompilationSession):
             ),
         ]
 
-    def __init__(
-            self, working_directory: Path, action_space: ActionSpace, benchmark: Benchmark
-            ):
+    def __init__(self, working_directory: Path, action_space: ActionSpace, benchmark: Benchmark):
         super().__init__(working_directory, action_space, benchmark)
+        self.parsed_bench = BenchmarkUri.from_string(benchmark.uri)
+        self.baseline_size = None
+        self.baseline_runtime = None
+        self._binary_valid = False
+        self._src_copied = False
+        self._wd_valid = False
         logging.info("Started a compilation session for %s", benchmark.uri)
 
     def apply_action(self, action: Event) -> Tuple[bool, Optional[ActionSpace], bool]:
@@ -73,16 +80,65 @@ class GccPRCompilationSession(CompilationSession):
 
         # Insert pass into corresponding list and ask shuffler to generate a new space 
         # If new action space is empty - return True as first element
+        self._binary_valid = False
         return False, None, False
 
     def get_observation(self, observation_space: ObservationSpace) -> Event:
         logging.info("Computing observation from space %s", observation_space.name)
         if observation_space.name == "runtime":
-            return Event(double_value=100.101)
+            return Event(double_value=self.get_runtime())
         elif observation_space.name == "size":
-            return Event(int64_value=75570)
+            return Event(int64_value=self.get_size())
         else:
             raise KeyError(observation_space.name)
+
+    def copy_bench(self):
+        if not self._src_copied:
+            print(self.working_dir)
+            copytree(self.parsed_bench.path, self.working_dir.joinpath('bench'), dirs_exist_ok=True)
+            self._src_copied = True
+
+    def prep_wd(self):
+        if not self._wd_valid:
+            call('touch list1.txt list2.txt list3.txt', shell=True, cwd=self.working_dir.joinpath('bench'))
+            self._wd_valid = True
+
+    def get_baseline(self):
+        self.compile_baseline()
+        self.baseline_size = self.get_size()
+        self.baseline_runtime = self.get_runtime()
+
+    def compile_baseline(self):
+        self.copy_bench()
+        base_opt = " ".join(self.parsed_bench.params.get("base_opt", "-O2"))
+        src_dir = " ".join(self.parsed_bench.params.get("src_dir"))
+        build_arg = " ".join(self.parsed_bench.params.get("build"))
+        call(f'''$AARCH_GCC {arg} {build_arg} {src_dir}*.c -o bench.elf''', shell=True, cwd=self.working_dir.joinpath('bench'))
+        self._binary_valid = True
+
+    def compile(self):
+        self.copy_bench()
+        self.prep_wd()
+        src_dir = " ".join(self.parsed_bench.params.get("src_dir"))
+        build_arg = " ".join(self.parsed_bench.params.get("build"))
+        plugin_args = "-fplugin-arg-plugin-pass_file=list1.txt -fplugin-arg-plugin-pass_file=list2.txt -fplugin-arg-plugin-pass_file=list3.txt"
+        call(f'''$AARCH_GCC -fplugin=$GCC_PLUGIN -fplugin-arg-plugin-pass_replace {plugin_args} {build_arg} {src_dir}*.c -o bench.elf''', shell=True, cwd=self.working_dir.joinpath('bench'))
+        self._binary_valid = True
+
+    def get_runtime(self):
+        if not self._binary_valid:
+            self.compile()
+        arg = " ".join(self.parsed_bench.params.get("run"))
+        start_time = clock_gettime(CLOCK_MONOTONIC)
+        run(f'qemu-aarch64 -L /usr/aarch64-linux-gnu ./bench.elf {arg}', shell=True, cwd=self.working_dir.joinpath('bench'), check=True)
+        end_time = clock_gettime(CLOCK_MONOTONIC)
+        return end_time - start_time
+
+    def get_size(self):
+        if not self._binary_valid:
+            self.compile()
+        return int(run('size bench.elf', shell=True, capture_output=True, cwd=self.working_dir.joinpath('bench')).stdout.split()[6])
+
 
 if __name__ == "__main__":
     create_and_run_compiler_gym_service(GccPRCompilationSession)
